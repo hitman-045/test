cb7e5d6153b0cb5e1e312f4524d5c75fa35ddd59
Hitman <38419085+hitman-045@users.noreply.github.com>
2021-08-22 16:40:32 +0530

    Update java.sh

--- java.sh
+++ java.sh
@@ -1,8 +1,6 @@
-sudo apt install openjdk-8-jdk-headless 
-sudo apt install openjdk-8-jre-headless
+sudo apt install openjdk-8-jdk-headless -y
+sudo apt install openjdk-8-jre-headless  -y
 java -version
 echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' | sudo tee -a /etc/environment 
 echo 'export JRE_HOME=/usr/lib/jvm/jre' | sudo tee -a /etc/environment 
 source /etc/environment
-echo $JAVA_HOME 
-echo $JRE_HOME 
